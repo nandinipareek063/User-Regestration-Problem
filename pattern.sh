e1bcaaf9124fdedda8ec231ea59f9bba243fe6fa
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-11 17:19:09 +0530

    Added: First name is valid or invalid is added

--- pattern.sh
+++ pattern.sh
@@ -1,1 +1,12 @@
 echo "Welcome to user regestration problem"
+
+#!/bin/bash
+echo "Welcome to User registration problem"
+read -p "Enter your first name " first
+pattern="^[A-Z]{1}[a-z]{2.}$";
+if [[ $first =~ $pattern ]]
+then
+        echo "First name is valid";
+else
+        echo "First name is invalid";
+fi
